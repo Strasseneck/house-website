ccb1bde12f5bd88a8bb5ee37fdc2d69520a84f49
Joe Copplestone <strasseneck@gmail.com>
2024-04-15 23:57:41 +0200

    Fix: player error

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b81e5805ce22ce0b")
+/******/ 		__webpack_require__.h = () => ("207a56b87edf2962")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/ui/player.tsx
+++ app/ui/player.tsx
@@ -1,10 +1,11 @@
 'use client'
+import Script from "next/script"
 
 export default function Player() {
     return (
         <div className="flex items-center justify-center">
             <div id='buzzsprout-large-player' className="w-5/6 "/>
-            <script type='text/javascript' src='https://www.buzzsprout.com/2319717.js?container_id=buzzsprout-large-player&player=large' />
+            <Script type='text/javascript' src='https://www.buzzsprout.com/2319717.js?container_id=buzzsprout-large-player&player=large' />
         </div>
     )
 }
